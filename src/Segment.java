dd72870e23a64ce722f632515239014a27d23ce4
Nezar <hamideh@yandex.ru>
2019-11-11 22:48:59 +0300

    v3

--- src/Segment.java
+++ src/Segment.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 public class Segment implements Figure {
     private R2Point p, q;
     private double radiusMin;
-    private Graphics g;
     public Segment(R2Point p, R2Point q) {
         this.p = p; this.q = q;
     }
@@ -24,7 +23,6 @@ public class Segment implements Figure {
     public void draw(Graphics g) {
         g.clearRect(-Drawing.width/2, -Drawing.height/2, Drawing.width, Drawing.height);
         g.drawLine(p.get_x(), p.get_y(), q.get_x(), q.get_y());
-        this.g=g;
     }
     public double radiusMinCircle(int x, int y){
         if(Math.sqrt(Math.pow(((Math.abs(p.get_x() - x))), 2) + Math.pow(((Math.abs(p.get_y() - y))), 2)) > Math.sqrt(Math.pow(((Math.abs(q.get_x() - x))), 2) + Math.pow(((Math.abs(q.get_y() - y))), 2)))
